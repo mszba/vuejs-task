7edde8bbd796a67a41028333a75167a35a90c58c
Marek Szuba <mszuba01@gmail.com>
2021-05-18 22:45:18 +0200

    add new props/tags/styles

--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,35 +1,14 @@
 <template>
   <div class="wrapper">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <div v-if="loading">
       <v-icon class="loadingIcon" name="circle-notch" scale="2.5" spin />
     </div>
-
-    <!-- <div
-      class="repoContainer"
-      v-for="(repo, index) in this.dataList"
-      :key="index"
-    >
-      <div class="dataWrap">
-        <span class="dataTitleLabel">Repository name:</span>
-        <h3 class="dataTitle">{{ repo.name }}</h3>
-      </div>
-      <div class="dataWrap">
-        <span class="dataTitleLabel">Commits list:</span>
-        <h3 class="dataTitle">asdasd asda sdsad a commit</h3>
-      </div>
-      <div class="dataWrap">
-        <span class="dataTitleLabel">Contributors list:</span>
-        <h3 class="dataTitle">asdasda sdsa dasdas</h3>
-      </div>
-    </div> -->
-
+    <div v-if="totalCount === 0">
+      <span class="noResults">no results</span>
+    </div>
     <div
       class="repoContainer"
-      v-for="(repo, index) in this.dataList"
+      v-for="(repo, index) in this.reposList"
       :key="index"
     >
       <div class="dataWrap">
@@ -72,22 +51,33 @@
       </div>
     </div>
 
-    <!-- <div class="repoContainer">
+    <a
+      class="userContainer"
+      v-for="(user, index) in this.usersList"
+      :key="index"
+      :href="`/user/${user.userProfile.login}`"
+    >
       <div class="userPresent">
-        <div class="userAvatar" />
-        <h3 class="userName">example Name</h3>
+        <img
+          class="userAvatar"
+          :src="`${user.userProfile.avatar_url}`"
+          :alt="`${user.userProfile.login}`"
+        />
+        <h3 class="userName">{{ user.userProfile.login }}</h3>
       </div>
 
-      <div class="userInfoWrap">
-        <span class="dataTitleLabel">Following:</span>
-        <h3 class="dataTitle">repo name vue-react12</h3>
-      </div>
+      <div class="userInfoContainer">
+        <div class="userInfoWrap">
+          <span class="dataTitleLabel">Followers:</span>
+          <h3 class="dataTitle">{{ user.userProfile.followers }}</h3>
+        </div>
 
-      <div class="userInfoWrap">
-        <span class="dataTitleLabel">Following:</span>
-        <h3 class="dataTitle">repo name vue-react12</h3>
+        <div class="userInfoWrap">
+          <span class="dataTitleLabel">Following:</span>
+          <h3 class="dataTitle">{{ user.userProfile.following }}</h3>
+        </div>
       </div>
-    </div> -->
+    </a>
   </div>
 </template>
 
@@ -96,22 +86,13 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop({ default: [], type: Array, required: true }) dataList!: [];
+  @Prop({ default: [], type: Array, required: true }) reposList!: [];
+  @Prop({ default: [], type: Array, required: true }) usersList!: [];
   @Prop({ default: false, type: Boolean, required: true }) loading!: boolean;
-  // contributorsList = [];
-  // commitsList = [];
-
-  mounted(): void {
-    // if (this.dataList.length > 0) {
-    //   this.contributorsList = this.dataList.contributors;
-    //   this.commitsList = this.dataList.commits;
-    //   console.log("contr", this.dataList);
-    // }
-  }
+  @Prop({ default: false, type: Number, required: true }) totalCount!: number;
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .wrapper {
   margin-top: 45px;
@@ -136,8 +117,31 @@ export default class HelloWorld extends Vue {
   border: 1px solid rgb(64 60 67 / 16%);
   box-shadow: 0 2px 10px 1px rgb(64 60 67 / 16%);
 }
+.userContainer {
+  padding: 15px 30px;
+  margin-bottom: 5px;
+  width: 100%;
+  min-height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  border-radius: 12px;
+  border: 1px solid rgb(64 60 67 / 16%);
+  box-shadow: 0 2px 10px 1px rgb(64 60 67 / 16%);
+  text-decoration: none;
+  color: #000;
+  transition: all 0.3s ease;
+}
+.userContainer:hover {
+  box-shadow: 0 2px 8px 1px rgb(64 60 67 / 24%);
+  border-color: rgba(223, 225, 229, 0);
+}
+.userInfoWrap {
+  padding: 0 40px;
+}
+
 .dataWrap {
-  /* width: 28%; */
   margin-bottom: 15px;
   display: flex;
   flex-direction: column;
@@ -157,23 +161,20 @@ export default class HelloWorld extends Vue {
   cursor: pointer;
   list-style: none;
 }
-/* .dataElementTitle + .dataElementTitle {
-  margin-right: 10px;
-} */
+
 .dataCommit {
   margin-left: 15px;
   font-size: 12px;
   list-style: none;
-  /* font-weight: bold; */
 }
 
 .dataCommit::before {
-  content: "\2022"; /* Add content: \2022 is the CSS Code/unicode for a bullet */
-  color: #118655; /* Change the color */
-  font-weight: bold; /* If you want it to be bold */
-  display: inline-block; /* Needed to add space between the bullet and the text */
-  width: 1em; /* Also needed for space (tweak if needed) */
-  margin-left: -1em; /* Also needed for space (tweak if needed) */
+  content: "\2022";
+  color: #118655;
+  font-weight: bold;
+  display: inline-block;
+  width: 1em;
+  margin-left: -1em;
 }
 
 .elementsList {
@@ -183,12 +184,18 @@ export default class HelloWorld extends Vue {
 
 .commitsList {
   max-height: 200px;
-  overflow-y: scroll;
+  overflow: scroll;
+}
+
+.userInfoContainer {
+  padding: 0 35px;
+  display: flex;
 }
 
 .userPresent {
   display: flex;
   align-items: center;
+  width: 200px;
 }
 
 .userAvatar {
@@ -196,16 +203,39 @@ export default class HelloWorld extends Vue {
   width: 80px;
   height: 80px;
   border-radius: 50%;
-  background: red;
 }
 .contributorAvatar {
   width: 50px;
   height: 50px;
   border-radius: 50%;
 }
-.abc {
-  display: -webkit-box;
-  -webkit-line-clamp: 4;
-  -webkit-box-orient: vertical;
+
+.userAvatar {
+  margin-right: 15px;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+}
+
+.noResults {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+@media screen and (max-width: 768px) {
+  .userInfoContainer {
+    padding: 0 15px;
+  }
+  .userInfoWrap {
+    padding: 0 20px;
+  }
+}
+@media screen and (max-width: 570px) {
+  .userContainer {
+    flex-direction: column;
+  }
+  .userInfoContainer {
+    margin-top: 10px;
+  }
 }
 </style>
