63d8a728849ce7e08cc954637802c334e1409528
Marek Szuba <mszuba01@gmail.com>
2021-05-18 22:45:38 +0200

    add User page

---
+++ src/views/User.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-wrapper">
+    <UserPage />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import UserPage from "../components/UserPage.vue";
+
+@Component({
+  components: {
+    UserPage,
+  },
+})
+export default class User extends Vue {}
+</script>
+
+<style scoped>
+.main-wrapper {
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
