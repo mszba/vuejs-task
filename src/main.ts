1d1d95da46b81015edbdafeb1d3ddcbaa0d7ee0b
Marek Szuba <mszuba01@gmail.com>
2021-05-18 22:41:51 +0200

    add moment imports and delete unnecessary ones

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import Icon from 'vue-awesome/components/Icon.vue';
+import VueMoment from 'vue-moment';
 import 'vue-awesome/icons';
 import './assets/global.css';
 
 Vue.config.productionTip = false;
 Vue.component('v-icon', Icon);
+Vue.use(VueMoment);
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
