3f92fcbf9d9654fcc3e6c4978cc4e705e3eb09ab
Marek Szuba <mszuba01@gmail.com>
2021-05-18 21:28:12 +0200

    add ts interfaces

---
+++ interface/index.ts
@@ -0,0 +1,41 @@
+export interface Data {
+  data: object;
+  total_count: number;
+  items: [];
+}
+
+export interface Items {
+  name: string;
+  commits_url: string;
+  contributors_url: string;
+}
+
+export interface Commits {
+  commit: {
+    [message: string]: string;
+  };
+}
+
+export interface contributorsTypes {
+  contributorName: string;
+  contributorAvatar: string;
+}
+
+export interface contributorObject {
+  login: string;
+  avatar_url: string;
+}
+
+export interface reposArray {
+  name: string;
+  commits: string[];
+  contributors: contributorsTypes[];
+}
+
+export interface usersInterface {
+  url: string;
+}
+
+export interface usersArrayInterface {
+  userProfile: object;
+}
